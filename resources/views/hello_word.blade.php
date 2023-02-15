1766e6a691635b29a2af1071449087859b593379
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-15 13:20:15 +0100

    completed

---
+++ resources/views/hello_word.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        </style>
+    </head>
+    <body>
+        <h1><em>Hello World</em></h1>
+    </body>
+</html>
